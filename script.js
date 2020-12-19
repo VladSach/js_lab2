89711a2071e21683e906db68fa39b273fa8f254d
Vlad <vladsach@gmail.com>
2020-12-19 15:40:44 +0200

    refactor

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+class Note {
+    constructor({id, title, body, date}, noteManager) {
+        this.el = null;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.date = new Date(date);
+        this.notesManager = noteManager;
+    }
+}
